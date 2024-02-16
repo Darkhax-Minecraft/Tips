1ef3b89c43c55d27a5245039d35f32b73f4ad58e
Paint_Ninja <PaintNinja@users.noreply.github.com>
2024-02-16 22:39:51 +0000

    Move `TipsModForgeClient` into `TipsModForge` to match Fabric

--- forge/src/main/java/net/darkhax/tipsmod/TipsModForge.java
+++ forge/src/main/java/net/darkhax/tipsmod/TipsModForge.java
@@ -1,6 +1,10 @@
 package net.darkhax.tipsmod;
 
 import net.darkhax.tipsmod.impl.Constants;
+import net.darkhax.tipsmod.impl.TipsModCommon;
+import net.darkhax.tipsmod.impl.client.TipRenderHandler;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
@@ -8,6 +12,12 @@ public class TipsModForge {
 
     public TipsModForge() {
 
-        TipsModForgeClient.init();
+        TipsModCommon.init();
+        MinecraftForge.EVENT_BUS.addListener(TipsModForge::drawScreen);
+    }
+
+    private static void drawScreen(ScreenEvent.Render.Post event) {
+
+        TipRenderHandler.drawTip(event.getGuiGraphics(), event.getScreen());
     }
 }
